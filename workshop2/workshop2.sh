132e024c97babea84a6b86da920dba059bbf4dbb
Bipin Kapali <bkapali@our.ecu.edu.au>
2018-04-02 22:02:04 +0800

    fetched uptime

--- workshop2/workshop2.sh
+++ workshop2/workshop2.sh
@@ -6,3 +6,8 @@ echo "${space// /-} System Report ${space// /-}" > workshop2_output.txt
 (
 	printf "Date: $(date +%d/%m/%Y)\tTime: $(date +%T)\tHost Name $(hostname)\n"
 )| column -t -s $'\t' >> workshop2_output.txt
+
+underline="$(printf '%0.1s' -{1..75})"
+echo "${underline// /-}" >> workshop2_output.txt
+
+uptime | awk -F'( |,|:)+' '{print "Uptime: ",$4,$5,",",$6,"hours,",$7,"minutes"}' >> workshop2_output.txt
