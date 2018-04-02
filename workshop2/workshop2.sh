35d4076b86b4a2b93a3674013cc4edb020bafbd4
Bipin Kapali <bkapali@our.ecu.edu.au>
2018-04-02 22:19:30 +0800

    memory utilisation and CPU load information added

--- workshop2/workshop2.sh
+++ workshop2/workshop2.sh
@@ -12,3 +12,6 @@ echo "${underline// /-}" >> workshop2_output.txt
 
 uptime | awk -F'( |,|:)+' '{print "Uptime: ",$4,$5,",",$6,"hours,",$7,"minutes"}' >> workshop2_output.txt
 uptime | awk -F'( |,|:)+' '{print "Current Users: ",$8}' >> workshop2_output.txt
+
+top -l 1 | head -n 10 | grep PhysMem | sed 's/, /n /g' | awk '{print "Memory Utilisation: ", substr($4,2)/$2*100,"%"}' >> workshop2_output.txt
+uptime | awk '{print "CPU Load: ",$10}' >> workshop2_output.txt
