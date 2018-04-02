d04b327b295c64461c63576608344e6affe27bfc
Bipin Kapali <bkapali@our.ecu.edu.au>
2018-04-02 18:47:14 +0800

    completed workshop1

--- workshop1/workshop1.sh
+++ workshop1/workshop1.sh
@@ -1,13 +1,14 @@
 echo "bash script to check the status of git repository"
 
- # examine git repository
- path=$1
- if [ ! -d "$path/.git" ]; then
- 	path="`pwd`"
- fi
- echo $path
+# examine git repository
+path=$1
+if [ ! -d "$path/.git" ]; then
+path="`pwd`"
+fi
+echo $path
 
- git log -1 --pretty=format:"most recent commit information
- author name= %aN
- author email= %ae
- comment=%s"
+echo "Number of commits= $(git rev-list --count HEAD)"
+git log -1 --pretty=format:"most recent commit information
+author name= %aN
+author email= %ae
+comment=%s"
