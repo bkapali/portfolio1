071adadcbe7d401131af8bbcf34c1a6fd91b38f9
Bipin Kapali <bkapali@our.ecu.edu.au>
2018-04-03 00:44:04 +0800

    Cheking online or offline

--- workshop3/workshop3.sh
+++ workshop3/workshop3.sh
@@ -40,3 +40,29 @@ then
 
  fi
 
+else
+
+	echo "You are off campus"
+     if [ $(id -u) -ne 0 ]; then
+         echo "need root user to run"
+         exit 1
+     fi
+     
+     gsettings set org.gnome.system.proxy mode 'none'
+
+     grep PATH /etc/environment > ecu.t
+     cat ecu.t > /etc/environment
+
+     printf "" > /etc/apt/apt.conf.d/95proxies
+     rm -rf ecu.t
+     
+     echo "Proxy settings have been cleared"
+     
+     if ping -c 1 www.github.com
+     then
+          echo "You are \"Online\""
+     else 
+          echo "You are \"Offline\""
+     fi
+fi
+
