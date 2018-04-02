553192e41259272cc24ae37c8d354b83fce48b18
Bipin Kapali <bkapali@our.ecu.edu.au>
2018-04-02 21:46:02 +0800

    Date, Time and host name Fetched

--- workshop2/workshop2.sh
+++ workshop2/workshop2.sh
@@ -1,3 +1,8 @@
 #bash script for desired output
 
 space="$(printf '%0.1s' -{1..30})"
+echo "${space// /-} System Report ${space// /-}" > workshop2_output.txt
+
+(
+	printf "Date: $(date +%d/%m/%Y)\tTime: $(date +%T)\tHost Name $(hostname)\n"
+)| column -t -s $'\t' >> workshop2_output.txt
